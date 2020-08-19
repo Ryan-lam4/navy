a85e094b255f4af8ee436c25fc7cd4fc783ed9da
Ryan Lamhaouar <ryan@Ryans-MacBook-Pro-2.local>
2020-08-19 16:20:33 -0400

    backround

--- guessingGame.js
+++ guessingGame.js
@@ -15,20 +15,87 @@
 
     var chosenQuestion = "";
 
-    var pickPerson = ["mikeFacts", "woodsFacts"];
+    var pickPerson = ["mike", "woods"];
 
+    var rightGuesses = 0
+    var wrongGuesses = 0
 
+    var rnewquestion = true
+
+//Listens for "Pick a New Fact" being clicked
 document.getElementById("newFact").addEventListener("click", function() {
-    chosenPerson = pickPerson[Math.floor(Math.random() * pickPerson.length)]
+    
+    //Allows only one question/answer at a time
+    if(rnewquestion == true) { 
+        rnewquestion = false
+    
+        //Chooses either "mike" or "woods"
+        chosenPerson = pickPerson[Math.floor(Math.random() * pickPerson.length)]
 
-    if(chosenPerson == "mikeFacts") {
-        chosenQuestion = mikeFacts[Math.floor(Math.random() * mikeFacts.length)]
+        //If the chosen person is Mr. Mike
+        if(chosenPerson == "mike") {
+
+            //Chooses random question in mikeFacts array
+            chosenQuestion = mikeFacts[Math.floor(Math.random() * mikeFacts.length)]
+            
+            //Listens for "Mr. Mike" being clicked
+            document.getElementById("mike").addEventListener("click", correct);
+            
+            //Listens for "Mr. Woods" being clicked
+            document.getElementById("woods").addEventListener("click", incorrect);
+        }
+
+        //If the chosen person is Mr. Woods
+        else if(chosenPerson == "woods") {
+
+            //Chooses random question in woodsFacts array
+            chosenQuestion = woodsFacts[Math.floor(Math.random() * woodsFacts.length)]
+            
+            //Listens for "Mr. Woods" being clicked
+            document.getElementById("woods").addEventListener("click", correct);
+
+            //Listens for "Mr. Mike" being clicked
+            document.getElementById("mike").addEventListener("click", incorrect);
+        }
+
+        //Displays question in the proper form
+        document.getElementById("question").innerHTML = "Who" + chosenQuestion + "?"
     }
-
-    else if(chosenPerson == "woodsFacts") {
-        chosenQuestion = woodsFacts[Math.floor(Math.random() * woodsFacts.length)]
-    }
-
-    document.getElementById("question").innerHTML = "Who" + chosenQuestion + "?"
-
 });
+
+function correct() {
+
+    alert("Correct!")    
+
+    //Increases Right Guess Counter
+    rightGuesses++
+    document.getElementById("right-guesses").innerHTML = rightGuesses
+
+    //Removes all event listeners
+    document.getElementById("mike").removeEventListener("click", correct);
+    document.getElementById("woods").removeEventListener("click", incorrect);
+    document.getElementById("mike").removeEventListener("click", incorrect);
+    document.getElementById("woods").removeEventListener("click", correct);
+
+    //Allows for a new question to be chosen
+    rnewquestion = true
+};
+
+function incorrect() {
+
+    console.log("incorrect")
+    alert("Incorrect!")
+
+    //Increases Wrong Guess Counter
+    wrongGuesses++
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses
+
+    //Removes all event listeners
+    document.getElementById("mike").removeEventListener("click", correct);
+    document.getElementById("woods").removeEventListener("click", incorrect);
+    document.getElementById("mike").removeEventListener("click", incorrect);
+    document.getElementById("woods").removeEventListener("click", correct);
+
+    //Allows for a new question to be chosen
+    rnewquestion = true
+};
